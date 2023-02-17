ebd933610740b2d0e9779d10f820b45559ccf7a6
james <james.anthony.russell36@gmail.com>
2023-02-17 22:00:12 +1100

    added token verification for the user

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,26 @@
-import express from 'express'
+import express from "express";
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 
 const router = express.Router()
 
+// check user is authenticated
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("hello user you are now logged in")
+})
 
-export default router;
+
+// UPDATE
+router.put("/:id", updateUser)
+
+// DELETE
+router.delete("/:id", deleteUser)
+
+// GET single hotel
+router.get("/:id", getUser)
+
+// GETALL
+router.get("/", getUsers)
+
+export default router
